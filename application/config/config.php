a0d1c66efa5c4b3160134009350475c90541a821
dedislamets <dedi.supatman@modena.co.id>
2021-03-28 14:56:21 +0700

    base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8080/ars/';
+// $config['base_url'] = 'http://localhost:8080/ars/';
+$config['base_url'] = 'http://ars.co.id/';
 
 
 /*
